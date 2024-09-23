6a3f458b15f79064d7e6fd73f09b5b250ef5f6fe
Kirill <146528280+VERBLYUDOK@users.noreply.github.com>
2024-09-23 11:51:34 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/OS-labs
-# Build directory: /workspaces/OS-labs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lab1")
-subdirs("tests")
--- build/tests/lab1_test[1]_tests.cmake
+++
@@ -1,7 +0,0 @@
-add_test([=[ParentProcessTest.CheckSumCalculation]=]  /workspaces/OS-labs/build/bin/lab1_test [==[--gtest_filter=ParentProcessTest.CheckSumCalculation]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[ParentProcessTest.CheckSumCalculation]=]  PROPERTIES WORKING_DIRECTORY /workspaces/OS-labs/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[ParentProcessTest.EmptyInput]=]  /workspaces/OS-labs/build/bin/lab1_test [==[--gtest_filter=ParentProcessTest.EmptyInput]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[ParentProcessTest.EmptyInput]=]  PROPERTIES WORKING_DIRECTORY /workspaces/OS-labs/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-add_test([=[ParentProcessTest.LargeNumbersInput]=]  /workspaces/OS-labs/build/bin/lab1_test [==[--gtest_filter=ParentProcessTest.LargeNumbersInput]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[ParentProcessTest.LargeNumbersInput]=]  PROPERTIES WORKING_DIRECTORY /workspaces/OS-labs/build/tests SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  lab1_test_TESTS ParentProcessTest.CheckSumCalculation ParentProcessTest.EmptyInput ParentProcessTest.LargeNumbersInput)
