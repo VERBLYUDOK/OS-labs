41e895b689dd2559efac435e75cfcf9d8c33aa0c
Kirbass <kirillasz3@gmail.com>
2024-12-03 22:49:38 +0300

    __cplusplus

--- lab4/include/functions.h
+++ lab4/include/functions.h
@@ -1,9 +1,15 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#ifdef __cplusplus
 extern "C" { // Совместимость для динамической загрузки
+#endif
+
     float Derivative(float A, float deltaX);
     int* Sort(int* array, int size);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif
